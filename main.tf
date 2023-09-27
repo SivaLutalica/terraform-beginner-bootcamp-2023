c3f19f5faa3da864d1554a1ce5a4dec8d422a613
Siva Lutalica <regigister@gmail.com>
2023-09-27 20:28:28 +0000

    #22 restructured project

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-  cloud {
-    organization = "grgicv-terraform-bootcamp-2023"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-provider "aws" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower   = true
@@ -39,4 +11,7 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
+    tags = {
+    UsrUuid = var.user_uuid
+  }
 }
