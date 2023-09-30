fcc5e5adc12a97c53ef3e418332dd9f9259acf83
SivaLutalica <regigister@gmail.com>
2023-09-30 19:38:38 +0200

    #28 enabled static website hositng uploaded index and error pages (#29)

--- main.tf
+++ main.tf
@@ -5,4 +5,6 @@ module "terrahouse_aws" {
 	source = "./modules/terrahouse_aws"
 	user_uuid = var.user_uuid
 	bucket_name = var.bucket_name
+	index_html_filepah = var.index_html_filepah
+	error_html_filepah = var.error_html_filepah
 }
