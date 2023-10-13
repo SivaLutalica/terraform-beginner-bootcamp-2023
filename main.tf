1dbfa9b3313d2cd8d880a650dd95416e0b4355cc
SivaLutalica <regigister@gmail.com>
2023-10-13 22:43:10 +0200

    #47 testing terra home on terratowns (#48)

--- main.tf
+++ main.tf
@@ -9,31 +9,26 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#	source = "./modules/terrahouse_aws"
-#	user_uuid = var.user_uuid
-#	bucket_name = var.bucket_name
-#	index_html_filepah = var.index_html_filepah
-#	error_html_filepah = var.error_html_filepah
-#	content_version = var.content_version
-#	assets_path = var.assets_path
-#}
+module "terrahouse_aws" {
+	source = "./modules/terrahouse_aws"
+	user_uuid = var.teacherseat_user_uuid
+	index_html_filepah = var.index_html_filepah
+	error_html_filepah = var.error_html_filepah
+	content_version = var.content_version
+	assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
+  name = "Krupa na vrbasu"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+Small town in Bosnia and Herzegovina that is worth a stop
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "the-nomad-pad"
   content_version = 1
 }
